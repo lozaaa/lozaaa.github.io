0e8eb0dbf32271f032b260f5e0ea1c35a12aebb6
Lozaaa <lozagebremariam1@gmail.com>
2018-08-01 12:52:05 -0700

    initial commit

---
+++ myScript.js
@@ -0,0 +1,6 @@
+alert("continue if u can handle the heartbreak #RipLi'lSeb");
+window.onload = function () {
+  document.getElementById('RIP').onclick = function () {pictureChange()}
+}
+
+document.getElementById('RIP').src = 'https://78.media.tumblr.com/3e289fd0dc234350605e2304866e56f4/tumblr_inline_nc4hg1BVvz1s2p2e6.png'
